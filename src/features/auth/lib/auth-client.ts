aa672c6984ad8194e5ca93cfbecb482baa303d4d
lespacito <lehackk192@gmail.com>
2025-12-10 13:14:31 +0100

    feat: Make authClient baseURL configurable via `BETTER_AUTH_URL` environment variable.

--- src/features/auth/lib/auth-client.ts
+++ src/features/auth/lib/auth-client.ts
@@ -2,7 +2,7 @@ import { usernameClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-	baseURL: "http://localhost:3001",
+	baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
 	redirectTo: "/",
 	plugins: [usernameClient()],
 });
