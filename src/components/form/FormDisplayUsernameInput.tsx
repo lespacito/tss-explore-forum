6da208667a145c59a48755477dcbd26f287e68bd
lespacito <30557423+lespacito@users.noreply.github.com>
2026-08-12 01:45:26 +0200

    Stabilize development tooling and forms

--- src/components/form/FormDisplayUsernameInput.tsx
+++ src/components/form/FormDisplayUsernameInput.tsx
@@ -9,6 +9,11 @@ export function FormDisplayUsernameInput(props: FormControlProps) {
 			<Input
 				id={field.name}
 				name={field.name}
+				aria-invalid={
+					props["aria-invalid"] ||
+					(field.state.meta.isTouched && !field.state.meta.isValid) ||
+					undefined
+				}
 				value={field.state.value}
 				onChange={(e) => field.handleChange(e.target.value)}
 				onBlur={field.handleBlur}
